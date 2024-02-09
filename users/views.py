79d86799a6bfb0b413b97245a23f907afb6a839f
Madeline Mclean <madeline.joanna.mclean@gmail.com>
2024-02-09 12:54:38 +0000

    user registration

--- users/views.py
+++ users/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpRequest
+from django.shortcuts import render, redirect
+from .forms import UserRegisterForm
+from django.contrib import messages
 
 
-def register(request):
-    form = UserCreationForm()
+def register(request: HttpRequest):
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get("username")
+            messages.success(request, f"Account created for {username}")
+            return redirect("blog-home")
+    else:
+        form = UserRegisterForm()
     return render(request, "users/register.html", {"form": form})
